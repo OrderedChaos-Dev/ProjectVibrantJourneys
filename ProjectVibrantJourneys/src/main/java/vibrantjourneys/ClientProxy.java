b92fff65642e610fe9b99cc34eb60b622d9bcfc1
mc-jt9 <modding.jt9@gmail.com>
2018-07-10 22:57:54 +0800

    TEST

---
+++ ProjectVibrantJourneys/src/main/java/vibrantjourneys/ClientProxy.java
@@ -0,0 +1,25 @@
+package vibrantjourneys;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import vibrantjourneys.util.Reference;
+
+public class ClientProxy extends CommonProxy
+{
+	@Override
+	public <T extends Entity> void registerEntityRenderer(Class<T> entity, IRenderFactory<? super T> factory)
+	{
+		RenderingRegistry.registerEntityRenderingHandler(entity, factory);
+	}
+	
+	@Override
+	public void registerItemRenderer(Item item)
+	{
+		ModelLoader.setCustomModelResourceLocation(item, 0, 
+				new ModelResourceLocation(Reference.MOD_ID + ":" + item.delegate.name()));
+	}
+}
