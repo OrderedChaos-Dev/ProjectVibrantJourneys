d2f99337186633cb5f06b0335d44305220b9ce03
OrderedChaos-Dev <jt896@scarletmail.rutgers.edu>
2021-05-17 19:27:31 -0400

    added more compostables

--- src/main/java/projectvibrantjourneys/init/PVJVanillaIntegration.java
+++ src/main/java/projectvibrantjourneys/init/PVJVanillaIntegration.java
@@ -11,10 +11,16 @@ public class PVJVanillaIntegration {
 	
 	public static void init() {
 		setCompostInfo(PVJBlocks.fallen_leaves, 0.1F);
+		setCompostInfo(PVJBlocks.twigs, 0.1F);
 		setCompostInfo(PVJBlocks.pinecones, 0.1F);
 		setCompostInfo(PVJBlocks.sea_oats, 0.65F);
 		setCompostInfo(PVJBlocks.cattail, 0.65F);
 		setCompostInfo(PVJBlocks.bark_mushroom, 0.4F);
+		setCompostInfo(PVJBlocks.fallen_leaves, 0.1F);
+		setCompostInfo(PVJBlocks.glowcap, 0.65F);
+		setCompostInfo(PVJBlocks.crimson_nettle, 0.65F);
+		setCompostInfo(PVJBlocks.warped_nettle, 0.65F);
+		setCompostInfo(PVJBlocks.short_grass, 0.25F);
 	}
 	
 	public static void setCompostInfo(IItemProvider item, float chance) {
