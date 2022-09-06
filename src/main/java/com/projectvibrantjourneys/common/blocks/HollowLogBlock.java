ad5fd61d830f124462c042fb5517740b57ba25fa
OrderedChaos-Dev <jt896@scarletmail.rutgers.edu>
2022-09-05 22:37:52 -0700

    made hollow logs waterloggable

--- src/main/java/com/projectvibrantjourneys/common/blocks/HollowLogBlock.java
+++ src/main/java/com/projectvibrantjourneys/common/blocks/HollowLogBlock.java
@@ -1,20 +1,26 @@
 package com.projectvibrantjourneys.common.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class HollowLogBlock extends RotatedPillarBlock {
+public class HollowLogBlock extends RotatedPillarBlock implements SimpleWaterloggedBlock {
 	
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	protected static final VoxelShape BOTTOM = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
 	protected static final VoxelShape TOP = Block.box(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 	protected static final VoxelShape LEFT_X = Block.box(0.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
@@ -27,6 +33,7 @@ public class HollowLogBlock extends RotatedPillarBlock {
 	
 	public HollowLogBlock(BlockBehaviour.Properties props) {
 		super(props);
+		this.registerDefaultState(this.defaultBlockState().setValue(AXIS, Direction.Axis.Y).setValue(WATERLOGGED, false));
 	}
 
 	@Override
@@ -40,5 +47,24 @@ public class HollowLogBlock extends RotatedPillarBlock {
 			case Y:
 				return SHAPE_Y;
 		}
+	}
+	
+	@Override
+	public BlockState updateShape(BlockState state, Direction direction, BlockState state2, LevelAccessor level, BlockPos pos, BlockPos pos2) {
+		if (state.getValue(WATERLOGGED)) {
+			level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+		}
+		
+		return state;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(AXIS, WATERLOGGED);
+	}
+	
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
 	}
 }
